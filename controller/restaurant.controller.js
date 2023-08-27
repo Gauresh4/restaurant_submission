9115414cb30ddc91238d45e790531364df393736
Gauresh <gaureshkumbhar2504@gmail.com>
2023-08-27 21:22:13 +0530

    Created 5 api endpoints for the restaurants

--- controller/restaurant.controller.js
+++ controller/restaurant.controller.js
@@ -45,7 +45,80 @@ exports.addRestaurant = async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).send({
-      message: "Some error occurred while creating the Restaurant.",
+      message: "Some error occurred while creating the Restaurant",
+    });
+  }
+};
+
+exports.getRestaurants = async (req, res) => {
+  try {
+    const restaurants = await Restaurant.find({});
+
+    res.status(200).send({
+      restaurants,
+      message: "Restaurants fetched successfully.",
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Some error occured while fetching the Restaurants",
+    });
+  }
+};
+
+exports.getRestaurantCategories = async (req, res) => {
+  try {
+    const categories = await Restaurant.distinct("category");
+    res.status(200).send(categories);
+  } catch (error) {
+    res.status(500).send({
+      message: "Some error occured while fetching Categories",
+    });
+  }
+};
+
+exports.getRestaurantsByCategoryName = async (req, res) => {
+  try {
+    // This API returns details of all the restaurants of a particular category in the database.
+    const category = req.params.category;
+    const restuarants = await Restaurant.find({ category });
+    res.status(200).send(restuarants);
+  } catch (error) {
+    res.status(500).send({
+      message: "Some error occured while fetching the Restaurant",
+    });
+  }
+};
+
+exports.getRestaurantById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const restaurant = await Restaurant.findById(id);
+
+    if (!restaurant)
+      return res.status(404).send({
+        message: "No Restaurant found with the given ID",
+      });
+
+    res.status(200).send(restaurant);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "Some error occured while fetching the Restaurant",
+    });
+  }
+};
+
+exports.getRestaurantsByRating = async (req, res) => {
+  try {
+    const rating = req.params.rating;
+    const restaurants = await Restaurant.find({
+      rating: { $gte: parseFloat(rating) },
+    });
+
+    res.status(200).send(restaurants);
+  } catch (error) {
+    res.status(500).send({
+      message: "Some error occured while fetching the Restaurant",
     });
   }
 };
--- routes/restaurant.route.js
+++ routes/restaurant.route.js
@@ -2,4 +2,18 @@ const restaurantController = require("../controller/restaurant.controller");
 
 module.exports = function (app) {
   app.post("/api/restaurant/add", restaurantController.addRestaurant);
+  app.get("/api/restaurant", restaurantController.getRestaurants);
+  app.get(
+    "/api/restaurant/categories",
+    restaurantController.getRestaurantCategories
+  );
+  app.get(
+    "/api/restaurant/categories/:category",
+    restaurantController.getRestaurantsByCategoryName
+  );
+  app.get("/api/restaurant/:id", restaurantController.getRestaurantById);
+  app.get(
+    "/api/restaurant/rating/:rating",
+    restaurantController.getRestaurantsByRating
+  );
 };
